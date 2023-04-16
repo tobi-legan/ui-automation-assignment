05fbf1f358c9a54717138bcd442bc0dc91939665
Oluwatobi Adelegan <tobiadelegan70@gmail.com>
2023-04-16 04:02:46 +0100

    install mocha generator for marge

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     reportFilename: '[name].html',
     overwrite: true,
     html: true,
-    json: false,
+    json: true,
   },
     blockHosts: [],
     chromeWebSecurity: false,
